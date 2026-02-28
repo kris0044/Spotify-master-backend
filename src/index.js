f26ebbc3d2bc8f3c2bd622f95fd46a2ccfba4643
kris0044 <krismistry74@gmail.com>
2026-02-28 11:17:50 +0530

    endpoint updates

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ initializeSocket(httpServer);
 
 app.use(
 	cors({
-		origin: "*",
+		origin: "https://spotify-master.onrender.com/",
 		credentials: true,
 	})
 );
@@ -92,7 +92,7 @@ app.use("/api/artist", artistRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
+	app.get("https://spotify-master.onrender.com/", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 	});
 }
